495611d521f65a1feede6773f864448d6368de00
Noah Porter <137841765+DieKatze1641@users.noreply.github.com>
2026-05-17 19:17:47 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -46,14 +46,6 @@ export default {
                             <div class="type-title-sm">Points when completed</div>
                             <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
                         </li>
-                        <li>
-                            <div class="type-title-sm">ID</div>
-                            <p>{{ level.id }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
-                        </li>
                     </ul>
                     <h2>Records</h2>
                     <table class="records">
